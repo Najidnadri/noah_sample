00f9f1ac85b6642f2373cf7ab4440506236545c6
Najidnadri <muhd.najid.nadri@gmail.com>
2023-03-18 13:42:28 +0800

    added mobile responsive to bestseller and about us section

--- sections/home/About.vue
+++ sections/home/About.vue
@@ -7,12 +7,12 @@
         </div>
 
         <div class="about-content w-100% px-20px">
-            <div class="left-part flex flex-col justify-between items-start">
-                <div class="grid grid-cols-[1fr_1fr] font-courier secondary opacity-90 text-16px gap-10px">
+            <div class="left-part flex flex-col justify-between items-start gap-30px">
+                <div class="about-description grid grid-cols-[1fr_1fr] font-courier secondary opacity-90 text-16px gap-10px">
                     <p>We are not a sustainable company. There's no such thing, but that doesn't mean we shouldn't try. Because if everyone tries, we're all better off</p>
                     <p>We are inspired by what we love-the culture and people in music, skateboarding, creativity, surfing, social and environmental justice, and New York.</p>
                 </div>
-                <div class="w-300px h-150px">
+                <div class="w-100% max-w-300px h-150px">
                     <img class="w-100% h-100% object-cover grayscale-100" src="https://cdn.shopify.com/s/files/1/1007/8070/articles/LOUIE-NOAH-49.jpg?v=1678541972&width=500"/>
                 </div>
             </div>
@@ -33,7 +33,50 @@ export default {
 <style scoped>
 .about-content {
     display: grid;
-    grid-template-columns: 1fr 1fr;
     gap: 20px;
+}
+</style>
+
+<style scoped>
+.about-content {
+    grid-template-columns: auto;
+}
+
+.left-part p {
+    font-size: 15px;
+}
+
+.about-description {
+    grid-template-columns: 1fr;
+    grid-template-rows: auto auto;
+}
+
+.right-part {
+    display: none;
+}
+
+
+
+
+@media only screen and (min-width: 340px) {
+.about-description {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto;
+}
+}
+
+@media only screen and (min-width: 600px) {
+
+.about-content {
+    grid-template-columns: 1fr 1fr;
+}
+
+.left-part p {
+    font-size: 16px;
+}
+
+.right-part {
+    display: block;
+}
 }
 </style>
--- sections/home/Bestsellers.vue
+++ sections/home/Bestsellers.vue
@@ -6,7 +6,8 @@
             <nuxt-link :to="''" class="text-16px decoration-underline">See more +</nuxt-link>
         </div>
 
-        <div class="bestsellers-content mt-20px w-100% px-20px">
+        <!-- Desktop -->
+        <div class="desktop bestsellers-content mt-20px w-100% px-20px">
             <div class="bestsellers w-100%">
                 <div class="bestseller w-100% flex justify-center items-center p-20px">
                     <div class="item w-100% cursor-pointer">
@@ -32,12 +33,67 @@
                 </div>
             </div>
         </div>
+
+        <!-- Mobile -->
+        <ClientOnly >
+            <Splide class="mobile bestsellers-content mt-20px w-100% px-20px splide" :options="splideOption" :has-track="false" @splide:mounted="splideMountedMove" @splide:move="splideMountedMove">
+                <SplideTrack>
+
+                    <SplideSlide class="slide w-100% h-100% overflow-hidden bestseller flex justify-center items-center p-20px pb-0px">
+                        <div class="item w-100% cursor-pointer h-100% grid grid-rows-[auto_auto]">
+                            <div class="item-header">
+                                <h3 class="text-16px font-550">NOAH X JESUS PIECE RUGBY</h3>
+                                <p class="text-16px opacity-90  font-courier">$188</p>
+                            </div>
+                            <div class="item-img  mx-auto mt-20px">
+                                <img src="https://cdn.shopify.com/s/files/1/1007/8070/products/S068SS23_SHORT_SLEEVE_FLANNEL_0450_1000x.jpg?v=1677886843" class="w-100% h-100% object-cover"/>
+                            </div>
+                        </div>                    
+                    </SplideSlide>
+
+                    <SplideSlide class="slide w-100% h-100% overflow-hidden bestseller flex justify-center items-center p-20px pb-0px">
+                        <div class="item w-100% cursor-pointer h-100% grid grid-rows-[auto_auto]">
+                            <div class="item-header">
+                                <h3 class="text-16px font-550">COUNTRY JACKET</h3>
+                                <p class="text-16px opacity-90  font-courier">$798</p>
+                            </div>
+                            <div class="item-img mt-20px mx-auto">
+                                <img src="https://cdn.shopify.com/s/files/1/1007/8070/products/KN110SS23_RUGBY_POCKET_CARDIGAN_0636_1000x.jpg?v=1678308267" class="w-100% h-100% object-cover"/>
+                            </div>
+                        </div>                    
+                    </SplideSlide>
+
+                </SplideTrack>
+
+                <div class="my-slider-progress">
+                    <div class="my-slider-progress-bar" ref="bar"></div>
+                </div>
+
+            </Splide>
+        </ClientOnly>
     </div>
 </template>
 
 <script>
 export default {
+    setup() {
+        let bar = ref(null);
+        let splideOption = {
+            pagination: false,
+            perPage: 1,
+            hasTrack: false,
+            arrows: false
+        };
 
+        let splideMountedMove = (splideEvt) => {
+            var end  = splideEvt.Components.Controller.getEnd() + 1;
+            var rate = Math.min( ( splideEvt.index + 1 ) / end, 1 );
+            bar.value.style.width = String( 100 * rate ) + '%';
+        }
+
+
+        return { splideOption, bar, splideMountedMove }
+    }
 }
 </script>
 
@@ -49,28 +105,109 @@ export default {
     place-items: center;
 }
 
-.bestseller {
+.desktop .bestseller {
     aspect-ratio: 1 / 1;
     grid-auto-rows: 1fr;
     max-height: 500px;
 }
 
-.item {
+.desktop .item {
     aspect-ratio: 1 / 1;
     max-width: 450px;
     max-height: 450px;
 }
 </style>
 
+
 <style scoped>
-.bestseller:nth-child(1) {
+.desktop .bestseller:nth-child(1) {
     border-top: 2px solid rgba(0, 0, 0, 0.5);
     border-right: 2px solid rgba(0, 0, 0, 0.5);
     border-bottom: 2px solid rgba(0, 0, 0, 0.5);
 }
 
-.bestseller:nth-child(2) {
+.desktop .bestseller:nth-child(2) {
     border-top: 2px solid rgba(0, 0, 0, 0.5);
     border-bottom: 2px solid rgba(0, 0, 0, 0.5);
 }
+
+.mobile .bestseller {
+    border-top: 2px solid rgba(0, 0, 0, 0.5);
+    border-bottom: 2px solid rgba(0, 0, 0, 0.5);
+}
+
+
+</style>
+
+<style scoped>
+.mobile {
+    display: block;
+}
+
+.desktop {
+    display: none;
+}
+
+.mobile .item-img {
+    width: 100%;
+    height: 100%;
+    margin-top: 0;
+}
+
+@media only screen and (min-width: 350px) {
+.mobile .item-img {
+    width: 85%;
+    height: 85%;
+    margin-top: 20px
+}
+}
+
+@media only screen and (min-width: 600px) {
+.desktop {
+    display: block;
+}
+
+.mobile {
+    display: none;
+}
+}
+</style>
+
+
+<style scoped>
+.splide__arrows.custom .splide__arrow {
+    opacity: 0.8;
+    border-radius: 0;
+    background: none;
+    top: 50%;
+    display: flex;
+}
+
+.splide__arrows.custom .splide__arrow:hover {
+    opacity: 1;
+}
+
+.splide__arrows.custom .splide__arrow--prev {
+    left: 3em
+}
+
+.splide__arrows.custom .splide__arrow--next {
+    right: 3em;
+}
+
+.my-slider-progress {
+    width: 100%;
+    max-width: 100px;
+    height: 4px;
+    margin: 0 auto;
+    margin-top: 20px;
+    background: #ccc;
+  }
+  
+  .my-slider-progress-bar {
+    background: var(--secondary-color);
+    height: 4px;
+    transition: width 400ms ease;
+    width: 0;
+  }
 </style>
