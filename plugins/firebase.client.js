6b5220bac5db8ba05977766e5ef8c15332f55698
Najidnadri <muhd.najid.nadri@gmail.com>
2023-03-18 14:45:35 +0800

    deployed to firebase

--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -1,20 +1,19 @@
 
-// import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 // import { getFirestore } from "firebase/firestore";
 // import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB7iloOyY7rkIdNR7i6yKNx8HpcNWk5M-c",
-//     authDomain: "penggerak-malaysia-official.firebaseapp.com",
-//     projectId: "penggerak-malaysia-official",
-//     storageBucket: "penggerak-malaysia-official.appspot.com",
-//     messagingSenderId: "249755658316",
-//     appId: "1:249755658316:web:e99e5fa05f3585c71d6ce9",
-//     measurementId: "G-3HVQQFTGV9"
-//   };
+const firebaseConfig = {
+  apiKey: "AIzaSyC_w9qNXinFsWX9ROl2xSyMNTY28oQlUqs",
+  authDomain: "sample-dd7b5.firebaseapp.com",
+  projectId: "sample-dd7b5",
+  storageBucket: "sample-dd7b5.appspot.com",
+  messagingSenderId: "231778919289",
+  appId: "1:231778919289:web:b38e50122828e9ce7785a8"
+};
 
-// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 // const analytics = getAnalytics(app);
 // const db = getFirestore(app);
 // const storage = getStorage(app);
